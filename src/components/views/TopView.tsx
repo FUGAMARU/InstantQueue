9e7c88cc261ac7463953c1ac6c484cc89ed02be0
FUGAMARU <gofugashi@gmail.com>
2025-05-28 01:42:25 +0900

    feat: ロゴ画像の設定

--- src/components/views/TopView.tsx
+++ src/components/views/TopView.tsx
@@ -46,12 +46,7 @@ export default component$(() => {
     <div class={styles.topView}>
       <div class={styles.main}>
         <div class={styles.upper}>
-          <img
-            alt="InstantQueue Logo"
-            height={80}
-            src="https://placehold.jp/80x80.png"
-            width={80}
-          />
+          <img height={80} src="/logo.svg" width={80} />
           <h1 class={styles.title}>InstantQueue</h1>
           <span class={styles.description}>Mashup Multiple Playlists, One Perfect Queue.</span>
         </div>
